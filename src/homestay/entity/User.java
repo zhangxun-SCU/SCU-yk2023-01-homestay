7d05d4406b9900ec441aa83cc487b32e3aa35651
Hao Zheng <86995164+Leamonz@users.noreply.github.com>
2023-12-03 00:28:00 +0800

    Delete src directory

--- src/homestay/entity/User.java
+++
@@ -1,9 +0,0 @@
-package homestay.entity;
-
-public class User {
-    public String id;
-    public String password;
-    public String email;
-    public String priority;
-    public String permission;
-}
